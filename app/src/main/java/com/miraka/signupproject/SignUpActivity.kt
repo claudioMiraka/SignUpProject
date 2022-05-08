1573a7f289d90508383b244f0783d6573817a834
claudioMiraka <claudio.miraka1@gmail.com>
2022-05-08 12:35:31 -0400

    Rename MainActivity -> SignUpActivity

--- app/src/main/java/com/miraka/signupproject/SignUpActivity.kt
+++ app/src/main/java/com/miraka/signupproject/SignUpActivity.kt
@@ -5,9 +5,9 @@ import android.os.Bundle
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class MainActivity : AppCompatActivity() {
+class SignUpActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_sign_up)
     }
 }
