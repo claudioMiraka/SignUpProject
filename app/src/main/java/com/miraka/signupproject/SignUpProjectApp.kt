ac676cdffde4ee5d9f1bc8bd305681f93f4792eb
claudioMiraka <claudio.miraka1@gmail.com>
2022-05-08 12:26:58 -0400

    add Hilt DI dependencies

--- app/src/main/java/com/miraka/signupproject/SignUpProjectApp.kt
+++ app/src/main/java/com/miraka/signupproject/SignUpProjectApp.kt
@@ -1,8 +1,10 @@
 package com.miraka.signupproject
 
 import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
 import timber.log.Timber
 
+@HiltAndroidApp
 class SignUpProjectApp : Application() {
 
     override fun onCreate() {
